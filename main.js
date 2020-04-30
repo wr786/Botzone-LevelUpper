5f93550d32b17042642b15bf069adea87a8aeea3
wr786 <wr786@pku.edu.cn>
2020-04-30 13:07:13 +0800

    完善了说明

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ function sleep(millisecond) {
         })
     }
 
-for(var i=0; i<10000; i++) {
+for(var i=0; i<10000; i++) {	// 共进行多少次，可以获得次数*10的经验值。
     $("#txtDescription_bot").val(i);
     $("#btnInheritVersion").click();
     $("#frmCreateBot").submit();
-    await sleep(3000);
+    await sleep(5000);	// 时间间隔，一方面是为了防止自身网络问题卡死，一方面也是减小botzone服务器负担。
 }
